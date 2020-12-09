72d9a9f7b57d780510afae4759cb576b52bd5c6a
WQY9264 <https://github.com/WQY9264/git1.git>
2020-12-09 08:18:31 +0800

    home首页

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+   我是首页home
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
